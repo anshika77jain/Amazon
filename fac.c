645f04de4231458dc763aaebfc807bfdb062bfb9
jstanotherperson <81288386+jstanotherperson@users.noreply.github.com>
2021-10-01 07:51:51 +0530

    Update fac.c
    For Finding Factorial using Recursive Approach

--- fac.c
+++ fac.c
@@ -15,10 +15,9 @@ void main()
  
 int fact(int n)
 {
-    int i,f=1;
-    for(i=1;i<=n;i++)
-    {
-        f=f*i;
-    }
-    return f;
+    if(n==0||n==1)
+     return 1;
+ else
+  return n*fact(n-1);
+   
 }
